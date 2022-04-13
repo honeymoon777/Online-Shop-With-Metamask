4b2aacef62d52bf08a1699ed912dbd1444fc7676
adi kurniawan <adiwkrnwn@gmail.com>
2022-04-13 21:15:01 +0700

    first commit

--- src/main/java/projectakhirOpenStory/controller/HomeController.java
+++ src/main/java/projectakhirOpenStory/controller/HomeController.java
@@ -1,13 +1,14 @@
 package projectakhirOpenStory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import projectakhirOpenStory.repository.UserRepository;
 
-@RestController
+@Controller
 public class HomeController {
 
 	@Autowired
@@ -18,4 +19,57 @@ public class HomeController {
 
 		return "index";
 	}
+	
+	@GetMapping("/index")
+	private String home(Model model) {
+
+		return "index";
+	}
+	
+	@GetMapping("/contacts")
+	private String contacts(Model model) {
+
+		return "contacts";
+	}
+	
+	@GetMapping("/grid-listing-3")
+	private String explore(Model model) {
+
+		return "grid-listing-3";
+	}
+	
+	@GetMapping("/help")
+	private String help(Model model) {
+
+		return "help";
+	}
+	
+	@GetMapping("/login")
+	private String login(Model model) {
+
+		return "login";
+	}
+	
+	@GetMapping("/register")
+	private String register(Model model) {
+
+		return "register";
+	}
+	
+	@GetMapping("/author")
+	private String author(Model model) {
+
+		return "author";
+	}
+	
+	@GetMapping("/blog")
+	private String blog(Model model) {
+
+		return "blog";
+	}
+	
+	
+	
+	
+	
 }
